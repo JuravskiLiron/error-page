c7221b33415da2141da5719a56d0efe1cc982d2b
JuravskiLiron <juravskliron@gmail.com>
2024-06-26 22:50:29 +0300

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Errors.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult ThrowException()
+        {
+            throw new Exception("This is a test exception");
+        }
+
+
+        [Route("Home/Error")]
+        public IActionResult Error()
+        {
+            var exceptionHandlerPathFeature = HttpContext.Features.Get<IExceptionHandlerPathFeature>();
+
+            if(exceptionHandlerPathFeature?.Error != null)
+            {
+                _logger.LogError(exceptionHandlerPathFeature.Error, "An error occurred");
+            }
+
+            return View();
+        }
+    }
+}
---
+++ Middleware/ErrorHandlingMiddleware.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using System.Net;
+
+namespace Errors.Middleware
+{
+    public class ErrorHandlingMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ErrorHandlingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext httpcontex)
+        {
+            try
+            {
+                await _next(httpcontex);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(httpcontex, ex);
+            }
+        }
+
+        private Task HandleExceptionAsync(HttpContext context, Exception ex)
+        {
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+            var response = new {message = ex.Message, details = ex.StackTrace};
+
+            return context.Response.WriteAsync(JsonConvert.SerializeObject(response));
+        }
+
+    }
+}
